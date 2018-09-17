68f54f83a9dd681f48b2fb60f02b26408cbed75a
Fermín Leal Payá <32195819+ferminlp93@users.noreply.github.com>
2018-09-17 16:13:51 +0200

    Add files via upload

--- vagrant/scripts/install.sh
+++ vagrant/scripts/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash 
 
+
+#Descargar anaconda
 curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 
+#Instalar anaconda
 sha256sum Anaconda3-5.0.1-Linux-x86_64.sh
 
 bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p /home/vagrant/anaconda3
--- vagrant/scripts/install2.sh
+++ vagrant/scripts/install2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
 
+#Instalar gedit
+
 yes "yes" | sudo apt-get install gedit
+
+#instalar interfaz grafica
 
 yes "yes" | sudo apt-get --install-recommends install ubuntu-desktop
 
